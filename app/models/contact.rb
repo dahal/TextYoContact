d949543fd4e79f2c5bd913c305515d517afc2700
Puru Dahal <pdahal@me.com>
2014-10-14 13:38:44 -0700

    Add activerecord validations to validate presence of name, cellphone and uniqueness and format of cellphone number

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,5 @@
 class Contact < ActiveRecord::Base
+  validates :name, :cellphone, presence: true
+  validates :cellphone, uniqueness: true
+  validates_format_of :cellphone, :with => /\A(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}\z/
 end
