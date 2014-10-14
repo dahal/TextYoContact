2e243352df9747cc95a1f2af0cd860df3c19dd12
Puru Dahal <pdahal@me.com>
2014-10-14 15:00:36 -0700

    Define resources routes for contacts controller and create index and show routes

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,5 +1,29 @@
 class ContactsController < ApplicationController
+  before_action :set_contact, only: [:show, :edit, :update, :destroy]
+  # GET /contacts
   def index
+    @contacts = Contact.all
+  end
+
+  # GET /contacts/:id
+  def show
+    render json: @contact
+  end
+
+  # GET /contacts/new
+  def new
+  end
+
+  # GET /contacts/:id/edit
+  def edit
+  end
+
+  # PATCH/PUT /contacts/:id
+  def update
+  end
+
+  # DELETE /contacts/:id
+  def destroy
   end
 
   private
@@ -12,4 +36,4 @@ class ContactsController < ApplicationController
     def contact_params
       params.require(:contact).permit(:name, :cellphone)
     end
-end
+  end
