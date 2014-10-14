34bc3e6e3e1826cbaa0d44e18c7a748b6ade30ac
Puru Dahal <pdahal@me.com>
2014-10-14 14:23:09 -0700

    set_contact and contact_params as controller's private methods

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,4 +1,15 @@
 class ContactsController < ApplicationController
   def index
   end
+
+  private
+    # Set contact to be avalilable to multiple different routes
+    def set_contact
+      @contact = Contact.find(params[:id])
+    end
+
+    # White listing params for security reason.
+    def contact_params
+      params.require(:contact).permit(:name, :cellphone)
+    end
 end
