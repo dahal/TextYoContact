f1f8472028c0ef81497d687e3c00ecbe49282315
Puru Dahal <pdahal@me.com>
2014-10-14 22:58:54 -0700

    Bind and initialize click and clear buttons

--- app/assets/javascripts/message/controller.js
+++ app/assets/javascripts/message/controller.js
@@ -3,5 +3,20 @@ App.Message.Controller = function(views){
 
   this.initialize = function(){
     console.log('Message Controller Initialized')
+    this.bindTextButton()
+    this.bindClearButton()
+  }
+
+  this.bindTextButton = function(){
+    $('.textButton').click(function(e){
+      console.log('Text Button Clicked')
+    })
+  }
+
+  this.bindClearButton = function(){
+    $('#clearButton').click(function(e){
+      e.preventDefault()
+      console.log('Clear Button Clicked')
+    })
   }
 }
