ec8e25a11aecfd344f279aba77fbdd46c22f46d8
Puru Dahal <pdahal@me.com>
2014-10-14 20:52:28 -0700

    Arrance the contact on contact list by created_at desc order

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,4 +2,5 @@ class Contact < ActiveRecord::Base
   validates :name, :cellphone, presence: true
   validates :cellphone, uniqueness: true
   validates_format_of :cellphone, :with => /\A(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}\z/
+  default_scope {order('created_at DESC')}
 end
