0c9ffb2635d8975048585bac71be714e089dd82a
Puru Dahal <pdahal@me.com>
2014-10-14 21:43:25 -0700

    Cleanup unnecessary routes and controller methods associated with routes

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,13 +1,8 @@
 class ContactsController < ApplicationController
-  before_action :set_contact, only: [:edit, :update, :destroy]
+  
   # GET /contacts
   def index
     @contacts = Contact.all
-  end
-
-  # GET /contacts
-  def new
-    @contact = Contact.new
   end
 
   # POST /contacts
@@ -16,21 +11,4 @@ class ContactsController < ApplicationController
     render json: @contact
   end
 
-  # GET /contacts/:id/edit
-  def edit
-  end
-
-  # PATCH/PUT /contacts/:id
-  def update
-  end
-
-  # DELETE /contacts/:id
-  def destroy
-  end
-
-  private
-    # Set contact to be avalilable to multiple different routes
-    def set_contact
-      @contact = Contact.find(params[:id])
-    end
-  end
+end
